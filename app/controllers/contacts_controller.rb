3f280b5c6ac04bf725c125081ce36a7efa70046a
Said Maadan <saidmaadan@users.noreply.github.com>
2015-07-03 13:41:27 -0500

    Modified contact controller create method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,10 +13,10 @@ class ContactsController < ApplicationController
 
 			ContactMailer.contact_email(name, email, body).deliver
 			flash[:success] = 'Message sent succefully'
-			redirect_to contact_path
+			redirect_to new_contact_path
 		else
 			flash[:danger] = 'Error occured, message could not be sent.'
-			redirect_to contact_path
+			redirect_to new_contact_path
 		end
 	end
 
