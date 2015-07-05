bb5e2d38c6d2a40adb91d487967353e6693eb02c
Said Maadan <saidmaadan@users.noreply.github.com>
2015-07-05 13:12:48 -0500

    Defined before action that required user to select a plan before proceed

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,26 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+	before_filter :select_plan, only: :new
+  
+  def create
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
+          resource.save_with_payment
+        else
+          resource.save
+        end
+      end
+    end
+  end
 
-	def create
-		super do |resource|
-			if params[:plan]
-				resource.plan_id = params[:plan] #User.first.plan_id = 2
-				if resource.plan_id == 2
-					resource.save_with_payment
-				else
-					resource.save_with_payment
-				end
-			end
-		end
-	end
+  private
+
+  def select_plan
+  	unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+  		flash[:notice] = "Please select a plan to sign up."
+  		redirect_to root_url
+  	end
+  end
+  
 end
