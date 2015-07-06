593d8c6c9562b757a4cdec0722115756eaa830f0
Said Maadan <saidmaadan@users.noreply.github.com>
2015-07-05 23:24:49 -0500

    Defined users authorization with before action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,10 @@
 class ProfilesController < ApplicationController
+	before_action :authenticate_user!
+	before_action :only_current_user
 
 	def new
 		@user = User.find(params[:user_id])
-		@profile = @user.build_profile
+		@profile = Profile.new
 	end
 
 	def create
@@ -16,9 +18,30 @@ class ProfilesController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:user_id])
+		@profile = @user.profile
+	end
+
+	def update
+		@user = User.find(params[:user_id])
+		@profile = @user.profile
+		if @profile.update_attributes(profile_params) #@profile.update(profile_params)
+			flash[:success] = "Profile Updated"
+			redirect_to user_path(params[:user_id])
+		else
+			render action: :edit
+		end
+	end
+
 	private
 
 	def profile_params
 		params.require(:profile).permit(:first_name, :last_name, :contact_email, :phone_number, :job_title, :description)
+	end
+
+	def only_current_user
+		@user = User.find(params[:user_id])
+		redirect_to root_url unless @user == current_user
 	end
 end
